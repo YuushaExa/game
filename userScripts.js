d4cc8ddafdbcda7ee9d06339a9108cd67f32646f
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-12-02 01:54:56 +0300

    Update userScripts.js

--- userScripts.js
+++ userScripts.js
@@ -1,3 +1,10 @@
+function turnTextRed() {
+            document.body.style.color = 'red'; // Change the text color of the body
+        }
+
+        // Call the function when the page loads
+        window.onload = turnTextRed;
+
 const playerName = localStorage.getItem('PlayerName') || 'Traveler';
             currentEpisode.scenes.forEach(scene => {
                 scene.text = scene.text.replace(/{playerName}/g, playerName);
