332358b4afff40db6690ae54e89e47d5ee4aba6e
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-12-02 02:26:23 +0300

    Update userScripts.js

--- userScripts.js
+++ userScripts.js
@@ -6,11 +6,16 @@ function replacePlayerName(scene) {
      scene.text = scene.text.replace(/{red}(.*?)\{\/red}/g, '<span style="color: red;">$1</span>');
 }
 
+function red(scene) {
+     scene.text = scene.text.replace(/{red}(.*?)\{\/red}/g, '<span style="color: red;">$1</span>');
+}
+
 // Modify the loadScene function to include player name replacement
 function loadScene(sceneId) {
     const scene = currentEpisode.scenes.find(s => s.id === sceneId);
     if (scene) {
         replacePlayerName(scene); // Replace player name before displaying
+        red(scene);
         displayScene(currentEpisode, sceneId);
     }
 }
