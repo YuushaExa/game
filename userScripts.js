9dd090e266768cce42869a8fa55359e0d7ff2080
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-12-02 02:04:33 +0300

    Update userScripts.js

--- userScripts.js
+++ userScripts.js
@@ -1,11 +1,4 @@
-function turnTextRed() {
-            document.body.style.color = 'red'; // Change the text color of the body
-        }
-
-        // Call the function when the page loads
-        window.onload = turnTextRed;
-
-const playerName = localStorage.getItem('PlayerName') || 'Traveler';
-            currentEpisode.scenes.forEach(scene => {
-                scene.text = scene.text.replace(/{playerName}/g, playerName);
-            });
+function replacePlayerName(scene) {
+    const playerName = localStorage.getItem('PlayerName') || 'Traveler';
+    scene.text = scene.text.replace(/{playerName}/g, playerName);
+}
