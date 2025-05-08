62cd0d36e88f61e84dbef7e0b4caf82a43c48fc3
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-05-08 17:14:27 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,24 @@
 const gameData = {
+  enemies: {
+    poring: {
+        str: 5,
+        hp: 30,
+        drops: [
+            { item: "jelly", rate: 0.5 },  // 50% chance to drop jelly
+            { gold: [1, 5], rate: 0.8 },   // 80% chance to drop 1-5 gold
+            { exp: 10, rate: 1.0 }         // Always gives 10 EXP
+        ]
+    },
+    wolf: {
+        str: 12,
+        hp: 80,
+        drops: [
+            { item: "skin", rate: 0.3 },  // 30% chance to drop wolf skin
+            { gold: [5, 15], rate: 0.6 },  // 60% chance to drop 5-15 gold
+            { exp: 30, rate: 1.0 }         // Always gives 30 EXP
+        ]
+    }
+},
     scenes: {
         start_screen: {
             html: `
